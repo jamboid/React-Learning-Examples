b46d6faa8270ff1c25724087ed86c3002f1415a8
Jamie Boyd <enkizero@gmail.com>
2018-04-18 00:05:20 +0100

    Fixed leg name abbr titles

--- full-stack-recruitment-test-master/client/src/components/itinerary/Itinerary.js
+++ full-stack-recruitment-test-master/client/src/components/itinerary/Itinerary.js
@@ -13,14 +13,14 @@ class Itinerary extends React.Component {
           <figure className='itin__legCarrier'><img className="itin__legLogo" src="https://logos.skyscnr.com/images/airlines/favicon/EZ.png" alt="Easyjet" /></figure>
           <div className='itin__location'>
             <div className='itin__locTime'>07:00</div>
-            <abbr className='itin__locName' title="{this.props.content.outboundLeg.overallFromName}">{this.props.content.outboundLeg.overallFromNameAbbr}</abbr>
+            <abbr className='itin__locName' title={this.props.content.outboundLeg.overallFromName}>{this.props.content.outboundLeg.overallFromNameAbbr}</abbr>
           </div>
           <div className='itin__connector'>
             <img src={arrow} alt="to" />
           </div>
           <div className='itin__location'>
             <div className='itin__locTime'>08:30</div>
-            <abbr className='itin__locName' title="{this.props.content.outboundLeg.overallToName}">{this.props.content.outboundLeg.overallToNameAbbr}</abbr>
+            <abbr className='itin__locName' title={this.props.content.outboundLeg.overallToName}>{this.props.content.outboundLeg.overallToNameAbbr}</abbr>
           </div>
           <div className='itin__duration'>
             <div className='itin__time'>1h 30</div>
@@ -31,14 +31,14 @@ class Itinerary extends React.Component {
           <figure className='itin__legCarrier'><img className="itin__legLogo" src="https://logos.skyscnr.com/images/airlines/favicon/EZ.png" alt="Easyjet" /></figure>
           <div className='itin__location'>
             <div className='itin__locTime'>07:00</div>
-            <abbr className='itin__locName' title="{this.props.content.inboundLeg.overallFromName}">{this.props.content.inboundLeg.overallFromNameAbbr}</abbr>
+            <abbr className='itin__locName' title={this.props.content.inboundLeg.overallFromName}>{this.props.content.inboundLeg.overallFromNameAbbr}</abbr>
           </div>
           <div className='itin__connector'>
             <img src={arrow} alt="to" />
           </div>
           <div className='itin__location'>
             <div className='itin__locTime'>08:30</div>
-            <abbr className='itin__locName' title="{this.props.content.inboundLeg.overallToName}">{this.props.content.inboundLeg.overallToNameAbbr}</abbr>
+            <abbr className='itin__locName' title={this.props.content.inboundLeg.overallToName}>{this.props.content.inboundLeg.overallToNameAbbr}</abbr>
           </div>
           <div className='itin__duration'>
             <div className='itin__time'>1h 30</div>
